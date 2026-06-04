41c793983294648980d7fd430e5ba30c29825657
for2332-lgtm <for2332@gmail.com>
2026-06-05 03:28:48 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'oasis-wanja-v1';
+const ASSETS = ['wanja-voca-drill.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) =>
+    Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) =>
+      cached || fetch(e.request).catch(() => caches.match('wanja-voca-drill.html'))
+    )
+  );
+});
